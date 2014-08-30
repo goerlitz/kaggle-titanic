f85bd6404ddcd0c51cfb6a5757431b8f1e6dd515
Olaf <olaf.goerlitz@gmail.com>
2014-08-30 20:44:49 +0200

    variation of model parameters

--- R/model.r
+++ R/model.r
@@ -17,9 +17,10 @@ for (i in c("Pclass","SibSp","Parch")) {
 levels(data$Parch) <- levels(test$Parch) <- seq(0,9)
 levels(data$Embarked) <- levels(test$Embarked) <- c("", "C", "Q", "S")
 
-# replace NA in Age with reasonable default value
-data$Age[is.na(data$Age)] <- 0
-test$Age[is.na(test$Age)] <- 0
+# replace NA in Age/Fare with reasonable default value (0, mean?)
+data$Age[is.na(data$Age)] <- mean(data$Age, na.rm = TRUE)
+test$Age[is.na(test$Age)] <- mean(data$Age, na.rm = TRUE)
+test$Fare[is.na(test$Fare)] <- mean(data$Fare, na.rm = TRUE)
 
 # add cabin letter
 data$CabinLetter <- as.factor(gsub(" .*|[0-9]", "", data$Cabin))
@@ -48,16 +49,18 @@ withTree <- function(fol) {
 #  print(treeModel)
   treePred <- predict(treeModel, newdata=test, type = "class")
   print(table(treePred))
+  treePred
 }
 
 withForest <- function(fol) {
   writeLines(paste("training random forest:", deparse(fol)))
   forestModel <- randomForest(fol, data=data)
-  # print(forestModel)
-   print(importance(forestModel))
+  print(forestModel)
+  # print(importance(forestModel))
   # CAUTION: cannot predict with missing levels/ NA values -> data/test must have same level
   forestPred <- predict(forestModel, newdata=test, type = "class")
   print(table(forestPred))
+  forestPred
 }
 
 withSVM <- function(fol) {
@@ -65,6 +68,7 @@ withSVM <- function(fol) {
   svmModel <- svm(fol, data=data)
   svmPred <- predict(svmModel, newdata=test, type = "class")
   print(table(svmPred))
+  svmPred
 }
 
 # define factor combinations to be used for model learning
@@ -75,6 +79,7 @@ formulas <- c(
   formula(Survived ~ Sex + Pclass + Age + Parch + SibSp),
   formula(Survived ~ Sex + Pclass + Age + CabinLetter),
   formula(Survived ~ Sex + Pclass + Age + CabinLetter + Fare),
+  formula(Survived ~ Pclass + CabinLetter + Fare),
   formula(Survived ~ Sex + Pclass + Age + CabinLetter + Embarked),
   formula(Survived ~ Sex + Pclass + Age + Parch + SibSp + CabinLetter + Embarked),
   formula(Survived ~ Sex + Pclass + Age + Parch + SibSp + CabinLetter + Embarked + Fare),
@@ -90,3 +95,15 @@ for (f in formulas) withForest(f)
 
 # training svm
 for (f in formulas) withSVM(f)
+
+export <- function(filename, result) {
+  result <- cbind(test$PassengerId, as.data.frame(result))
+  names(result) <- c("PassengerId", "Survived")
+  result$Survived <- as.numeric(result$Survived) -1
+  write.table(result, filename, sep=",", row.names=FALSE)
+}
+
+#export("predictionSVM.csv", withSVM(formula(Survived ~ Sex + Age + Fare))) # score: 0.77033
+#export("predictionSVM.csv", withSVM(formula(Survived ~ Sex + Pclass + Age + Fare))) # score: 0.77512
+#export("predictionSVM.csv", withSVM(formula(Survived ~ Sex + Pclass + Age + CabinLetter + Fare))) # score: 0.76555
+export("predictionForest.csv", withForest(formula(Survived ~ Sex + Pclass + Age + Fare))) # score: 0.77990
